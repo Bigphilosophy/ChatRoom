15ed425bebd3368c756cac20cac7775dcc415bb8
xiongsyao <xiongshengyao@hotmail.com>
2018-02-10 20:53:40 +0800

    [feature]:抽象

--- prepare_file.py
+++ prepare_file.py
@@ -1,20 +1,14 @@
+# !/usr/bin/python3
 # -*- coding: utf-8 -*-
 import os
 
-def prepare_file():
-    with open("成语.txt", "w+") as to_file:
-        with open("成语大全.txt", "r") as f:
-            for line in f:
-                if line.find("拼音") != -1:
-                    strip_line = line.replace(" ", "")
-                    split_line = strip_line.split("拼音")[0] + "\n"
-                    to_file.write(split_line)
-
-
 class Person():
     def __init__(self, name, word_file_path):
-        self.name = name,
+        self.name = name
         self.word_file_path = word_file_path
+
+    def __repr__(self):
+        return self.name
 
     def say(self, words):
         message = "[{}]:{}".format(self.name, words)
@@ -32,20 +26,45 @@ class Person():
         self.say(resp)
         return resp
 
+    def choose_one(self):
+        import random
+        num = random.choice(range(30000))
+        i = 0
+        with open(word_file_path, "r") as f:
+            for line in f:
+                if i == num:
+                    resp = line.replace("\n", "")
+                    self.say(resp)
+                    return resp
+                i += 1
+            raise KeyError
+
+
+class Game():
+    def __init__(self, memebers):
+        self.memebers = memebers
+        self.endword = "I lose!"
+
+    def run(self):
+        import random
+        first = random.choice(self.memebers)
+        print("----[{}]先手-----".format(first))
+        self.memebers.remove(first)
+        sencond = self.memebers[0]
+        print("----game start----")
+        current_word = first.choose_one()
+        for i in range(100):
+            if current_word == self.endword: break
+            word_tail = current_word[-1]
+            current_word = sencond.fight(word_tail)
+            if current_word == self.endword: break
+            word_tail = current_word[-1]
+            current_word = first.fight(word_tail)
+
 
 if __name__ == "__main__":
-    prepare_file()
     word_file_path = "成语.txt"
     LiMing = Person("LiMing", word_file_path)
     HanMeimei = Person("HanMeimei", word_file_path)
-    print(LiMing.name)
-    # ----start
-    print("-----game begin------")
-    current_word = "为所欲为"
-    for i in range(10000):
-        if current_word == "I lose!": break
-        word_tail = current_word[-1]
-        current_word = HanMeimei.fight(word_tail)
-        if current_word == "I lose!": break
-        word_tail = current_word[-1]
-        current_word = LiMing.fight(word_tail)
+    game = Game([LiMing, HanMeimei])
+    game.run()
